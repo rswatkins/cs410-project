7a45662dbbdf15def67f459dca07e6dae22a3100
Ryan Watkins <watkinsr@my.ccsu.edu>
2019-11-02 17:30:46 -0400

    first commit

---
+++ app/routes.py
@@ -0,0 +1,20 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/results')
+def results():
+    return render_template('results.html')
+
+@app.route('/data')
+def data():
+    return render_template('data.html')
+
+@app.route('/parameters')
+def parameters():
+    return render_template('parameters.html')
