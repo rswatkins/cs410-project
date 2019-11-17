b8f9c6100d55c3d643b269bad7d00ab137354d25
Ryan Watkins <watkinsr@my.ccsu.edu>
2019-11-16 19:00:02 -0500

    Add basic file uploading

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
+import os
 from app import app
-from flask import render_template
+from flask import render_template, request
+
+app.config["UPLOAD_FOLDER"] = app.root_path + "/static/spreadsheets"
 
 @app.route('/')
 def index():
@@ -16,3 +19,10 @@ def data():
 @app.route('/parameters')
 def parameters():
     return render_template('parameters.html')
+
+@app.route('/upload', methods = ['POST'])
+def upload():
+    if request.method == 'POST':
+        spreadsheet = request.files['myFile']
+        spreadsheet.save(os.path.join(app.config["UPLOAD_FOLDER"],spreadsheet.filename))
+        return render_template('index.html')
